bc5e8f8588f309341f0899e487852cbf55df748e
SachinMate <sachin.mate@byjus.com>
2026-05-06 22:09:30 +0530

    quick check

--- cypress/e2e/1-getting-started/handling-dropdown.js
+++ cypress/e2e/1-getting-started/handling-dropdown.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Handling dropdowns', () => {
-    it('select option from doropdown', () => {
+    it('select options from doropdown', () => {
         cy.visit('https://webdriveruniversity.com')
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true})
         cy.get('#dropdowm-menu-1').select('Python').should('have.value', 'python')
